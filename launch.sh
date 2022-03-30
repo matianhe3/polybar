d7a1df473aa81be63c942597340ebb460b972c83
matianhe3 <matianhe3@gmail.com>
2022-03-31 06:28:36 +0800

    update

--- launch.sh
+++ launch.sh
@@ -4,8 +4,6 @@
 killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
-echo hook:module/ipc1 >>/tmp/polybar_mqueue.*
-polybar-msg [-p pid] hook ipc 1
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
